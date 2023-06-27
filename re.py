ce12981b5caf99c62471f94eedb80810a3052eea
hoangdai0202 <137849959+hoangdai0202@users.noreply.github.com>
2023-06-27 09:14:22 +0700

    Create re.py

---
+++ re.py
@@ -0,0 +1,7 @@
+echo "# hoang" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/hoangdai0202/hoang.git
+git push -u origin main
